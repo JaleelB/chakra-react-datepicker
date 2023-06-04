1a054a9bbce955987c7f499c34e63b5012b86134
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-06-04 15:13:11 -0400

    feat: navigation to select different calendar types

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Heading, StackDivider, VStack, Text, Link } from '@chakra-ui/react'
+import { Heading, Text, Box, Tab, TabList, TabPanel, TabPanels, Tabs, Flex } from '@chakra-ui/react'
 import DateTimePicker from './DateTimePicker'
 import { useState } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -13,27 +13,57 @@ function App() {
   };
 
   return (
-    <VStack
-      paddingX={{ base: '2rem', md: '8rem' }}
-      paddingY={{ base: '1rem', md: '4rem' }}
-      spacing={4}
+    <Box
+      paddingX={{ base: '2rem', md: '4rem', lg: '6rem' }}
+      paddingY={{ base: '64px', md: '96px' }}
       minHeight={'600px'}
       alignItems="flex-start"
-      divider={<StackDivider />}
-    >
-      <Heading>Chakra UI React Datepicker</Heading>
-        <Link
-          href="https://github.com/JaleelB/chakra-react-datepicker/blob/main/src/datepicker-styles.ts"
-          textUnderlineOffset="0.2rem"
-        >
-          🔗 Source code for date picker component chakra styling
-        </Link>
-      <Text>An implementation of the react date picker package with the elegance and simplicity of Chakra UI!</Text>
-      <DateTimePicker
-        onChange={handleDateChange}
-        calendarType='dateTime'
-      />
-    </VStack>
+    > 
+      <Text color="green" fontSize="1rem" fontWeight={500}>Demo</Text>
+      <Heading mb={4}>Chakra UI React Datepicker</Heading>
+      <Text mb={2}>An implementation of the react date picker package with the elegance and simplicity of Chakra UI!</Text>
+
+      <Box width="100%" mt={16}>
+        <Tabs variant="soft-rounded" colorScheme="green" isLazy>
+          <Flex width="100%" direction={{ base: 'column', md: 'row' }} gap={{ base: 0, md: 16 }}>
+            <TabList width={{base: "100%", md: "250px"}}>
+              <Flex width="100%" direction={{ base: 'column' }}>
+                <Tab justifyContent="flex-start" borderRadius={4}>Date</Tab>
+                <Tab justifyContent="flex-start" borderRadius={4}>Date Time</Tab>
+                <Tab justifyContent="flex-start" borderRadius={4}>Date Range</Tab>
+                <Tab justifyContent="flex-start" borderRadius={4}>Date Time Range</Tab>
+              </Flex>
+            </TabList>
+            <TabPanels>
+              <TabPanel borderLeft={{ base: 0, md: "1px" }} borderColor={{ base: "none", md: "gray.200" }} paddingLeft={{ base: 0, md: 16 }} paddingRight={0} paddingY={{ base: 8, md: 0 }}>
+                <DateTimePicker
+                  onChange={handleDateChange}
+                  calendarType='default'
+                />
+              </TabPanel>
+              <TabPanel borderLeft={{ base: 0, md: "1px" }} borderColor={{ base: "none", md: "gray.200" }} paddingLeft={{ base: 0, md: 16 }} paddingRight={0} paddingY={{ base: 8, md: 0 }}>
+                <DateTimePicker
+                  onChange={handleDateChange}
+                  calendarType='dateTime'
+                />
+              </TabPanel>
+              <TabPanel borderLeft={{ base: 0, md: "1px" }} borderColor={{ base: "none", md: "gray.200" }} paddingLeft={{ base: 0, md: 16 }} paddingRight={0} paddingY={{ base: 8, md: 0 }}>
+                <DateTimePicker
+                  onChange={handleDateChange}
+                  calendarType='dateRange'
+                />
+              </TabPanel>
+              <TabPanel borderLeft={{ base: 0, md: "1px" }} borderColor={{ base: "none", md: "gray.200" }} paddingLeft={{ base: 0, md: 16 }} paddingRight={0} paddingY={{ base: 8, md: 0 }}>
+                <DateTimePicker
+                  onChange={handleDateChange}
+                  calendarType='dateTimeRange'
+                />
+              </TabPanel>
+            </TabPanels>
+          </Flex>
+        </Tabs>
+      </Box>
+    </Box>
   )
 }
 
